c9d64264bf544773630d7638209afcd0adb10fdc
huriinalmazz <huriinalmazz@student.telkomuniversity.ac.id>
2022-12-26 12:12:09 +0700

    MODUL 3 OOP

---
+++ MODUL3_HURIN/src/Calculation.java
@@ -0,0 +1,29 @@
+public class Calculation implements Runnable{
+    double radius, side, area;
+    double phi = 3.14; 
+
+    public void setSquare(int lengthSquare){
+        this.lengthSquare = 4 * lengthSquare;
+    }
+
+    public void getSquare(){
+        return Square;
+    }
+
+    public void setCircle(){
+        this.area = 
+    }
+
+    public void getCircle(){
+        return 
+    }
+
+    public void setTrapezoid(){
+        
+    }
+
+    public void getTrapezoid(){
+        return 
+    }
+}
+
